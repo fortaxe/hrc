ffd9178cce8e55e74306672a01c01a4ee99146a6
amarnath666 <amarnathdhumal2001@gmail.com>
2025-08-26 11:59:27 +0530

    fixed text card

--- components/landing/text-card.tsx
+++ components/landing/text-card.tsx
@@ -11,7 +11,7 @@ const TextCard = ({
   firstDescription,
 }: TextCardProps) => {
   return (
-    <div className="flex flex-col p-4 sm:p-[30px] bg-[#181818] rounded-[10px] sm:rounded-[15px]">
+    <div className="flex flex-col p-4 sm:p-[30px] bg-[#181818] rounded-[10px] sm:rounded-[15px] w-full">
       
       <div>
         <p className="text-[#E1251B] text-[16px] sm:text-[16px] leading-[1] tracking-[0em]">{heading}</p>
@@ -21,21 +21,21 @@ const TextCard = ({
       </div>
      
       {firstHeading && firstDescription && (
-        <div className="pt-[25px] sm:pt-[30px]">
+        <div className="pt-[20px] sm:pt-[30px]">
           <p className="text-[16px] sm:text-[16px] leading-[1] tracking-[0em] text-[#F6F6EB]">{firstHeading}</p>
           <p className="description pt-[15px]">{firstDescription}</p>
         </div>
       )}
  
       {secondHeading && secondDescription && (
-        <div className={`${firstHeading && firstDescription ? '' : 'pt-[25px] sm:pt-[50px]'}`}>
+        <div className="pt-[20px] sm:pt-[30px]">
           <p className="text-[16px] sm:text-[16px] leading-[1] tracking-[0em] text-[#F6F6EB]">{secondHeading}</p>
           <p className="description pt-[15px]">{secondDescription}</p>
         </div>
       )}
      
       {thirdHeading && thirdDescription && (
-        <div className="pt-[25px] sm:pt-[30px]">
+        <div className="pt-[20px] sm:pt-[30px]">
           <p className="text-[16px] sm:text-[16px] leading-[1] tracking-[0em] text-[#F6F6EB]">{thirdHeading}</p>
           <p className="description pt-[15px]">{thirdDescription}</p>
         </div>
