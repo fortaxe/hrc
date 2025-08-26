c60d91b5ed94831337d12a039a76941287967a26
amarnath666 <amarnathdhumal2001@gmail.com>
2025-08-26 15:02:59 +0530

    made minor chanes

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -33,51 +33,49 @@ export function Hero() {
       </div>
 
       {/* Hero Content */}
-      <div className=" pt-[65px] sm:pt-[176px] ">
-        <div className="flex flex-1 justify-center items-center relative  max-w-[1440px] mx-auto w-full">
-          <motion.div
-            initial={{ opacity: 0.0, y: 40 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{
-              delay: 0.3,
-              duration: 0.8,
-              ease: "easeInOut",
-            }}
-            className="flex flex-col gap-5 sm:gap-[15px] max-w-[1440px] w-full  text-left px-[16px] sm:px-[30px]"
-          >
-            <div className="flex justify-between items-end ">
-              <div>
-                <p className="font-instrument-italic block sm:hidden text-[22px] leading-[0.46] tracking-[0] text-white pb-[130px] text-center">
-                  Hall B2, Booth E12
-                </p>
-                  <p className="text-[148px] md:text-[120px] xl:text-[230px] -ml-[13px] md:-ml-[21px] tracking-[-0.03em] text-[#E1251B] text-left leading-[106px] md:leading-[170px]">
-                  HRC
-                  <span className="font-instrument-italic text-white text-[57px] md:text-[120px] xl:text-[230px] ml-2 sm:ml-5 leading-[42px] md:leading-[170px]">
-                    at
-                  </span>
-                </p>
-              </div>
-              <div>
-                <p className="hidden sm:block xl:text-[36px] leading-[1] md:leading-[1] font-instrument-italic  tracking-[0] text-white   text-right ">
-                  Hall B2, Booth E12
-                </p>
-              </div>
-            </div>
-            <p className="text-[57px] md:text-[120px] xl:text-[230px] leading-[42px] md:leading-[170px]  tracking-[-0.03em] text-white  text-left uppercase md:-ml-[21px]">
-              IAA MOBILITY
-            </p>
+      <div className="flex flex-1 justify-center items-center w-full max-w-[1440px] mx-auto relative grow">
+        <motion.div
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="flex flex-col gap-5 sm:gap-[15px] max-w-[1440px] w-full text-left px-[16px] sm:px-[30px]"
+        >
+          <div className="flex justify-between items-end ">
             <div>
-              <div className="flex items-center flex-col sm:flex-row sm:justify-between gap-5 sm:gap-0 mt-[130px] sm:mt-0">
-                <p className="text-[22px] xl:text-[36px] leading-[1] md:leading-[1] font-instrument-italic  tracking-[0] text-white   text-left ">
-                  Press Day : 8 September 2025
-                </p>
-                <p className="text-[22px]  xl:text-[36px] leading-[1] md:leading-[1] font-instrument-italic  tracking-[0] text-white   text-right ">
-                  Industry Days :9-12 September 2025{" "}
-                </p>
-              </div>
+              <p className="font-instrument-italic block sm:hidden text-[22px] leading-[0.46] tracking-[0] text-white pb-[130px] text-center">
+                Hall B2, Booth E12
+              </p>
+              <p className="text-[148px] md:text-[120px] xl:text-[230px] -ml-[13px] md:-ml-[21px] tracking-[-0.03em] text-[#E1251B] text-left leading-[106px] md:leading-[170px]">
+                HRC
+                <span className="font-instrument-italic text-white text-[57px] md:text-[120px] xl:text-[230px] ml-2 sm:ml-5 leading-[42px] md:leading-[170px]">
+                  at
+                </span>
+              </p>
             </div>
-          </motion.div>
-        </div>
+            <div>
+              <p className="hidden sm:block xl:text-[36px] leading-[1] md:leading-[1] font-instrument-italic  tracking-[0] text-white   text-right ">
+                Hall B2, Booth E12
+              </p>
+            </div>
+          </div>
+          <p className="text-[57px] md:text-[120px] xl:text-[230px] leading-[42px] md:leading-[170px]  tracking-[-0.03em] text-white  text-left uppercase md:-ml-[21px]">
+            IAA MOBILITY
+          </p>
+          <div>
+            <div className="flex items-center flex-col sm:flex-row sm:justify-between gap-5 sm:gap-0 mt-[130px] sm:mt-0">
+              <p className="text-[22px] xl:text-[36px] leading-[1] md:leading-[1] font-instrument-italic  tracking-[0] text-white   text-left ">
+                Press Day : 8 September 2025
+              </p>
+              <p className="text-[22px]  xl:text-[36px] leading-[1] md:leading-[1] font-instrument-italic  tracking-[0] text-white   text-right ">
+                Industry Days :9-12 September 2025{" "}
+              </p>
+            </div>
+          </div>
+        </motion.div>
       </div>
     </div>
   );
